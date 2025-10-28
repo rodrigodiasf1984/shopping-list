5c63ae5eee1029c945b9bba00e15ae04721f1528
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2025-10-28 13:25:36 +0100

    chore: add state to handle add items

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,45 +1,65 @@
-import { View, Image, TouchableOpacity, Text, FlatList } from "react-native";
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  FlatList,
+  Alert,
+} from "react-native";
 import { styles } from "./styles";
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import Filter from "@/components/Filter";
 import { FilterStatus } from "@/types/FilterStatus";
 import { Item } from "@/components/Item";
+import { useState } from "react";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
-const ITEMS = [
-  {
-    id: "1",
-    status: FilterStatus.DONE,
-    description: "Pasta",
-  },
-  {
-    id: "2",
-    status: FilterStatus.PENDING,
-    description: "Salmon",
-  },
-  { id: "3", status: FilterStatus.PENDING, description: "Milk" },
-];
-
 export function Home() {
+  const [items, setItems] = useState<any>([]);
+  const [filter, setFilter] = useState(FilterStatus.PENDING);
+  const [description, setDescription] = useState("");
+
+  function handleAdd() {
+    if (!description.trim()) {
+      return Alert.alert("Adicionar", "Informe a descrição para adicionar.");
+    }
+
+    const newItem = {
+      id: Math.random().toString().substring(2),
+      description,
+      status: FilterStatus.PENDING,
+    };
+
+    setItems([...items, newItem]);
+  }
+
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
       <View style={styles.form}>
-        <Input placeholder="add an item to the list" />
-        <Button title="Add" />
+        <Input
+          placeholder="add an item to the list"
+          onChangeText={setDescription}
+        />
+        <Button title="Add" onPress={handleAdd} />
       </View>
       <View style={styles.content}>
         <View style={styles.header}>
-          {FILTER_STATUS.map((fs) => (
-            <Filter key={fs} status={fs} isActive />
+          {FILTER_STATUS.map((status) => (
+            <Filter
+              key={status}
+              status={status}
+              isActive={status === filter}
+              onPress={() => setFilter(status)}
+            />
           ))}
           <TouchableOpacity style={styles.clearButton}>
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
         <FlatList
-          data={ITEMS}
+          data={items}
           keyExtractor={(item) => item.id}
           showsVerticalScrollIndicator={false}
           ItemSeparatorComponent={() => <View style={styles.separator} />}
@@ -49,7 +69,7 @@ export function Home() {
           renderItem={({ item }) => (
             <Item
               onRemove={() => console.log("remove")}
-              onStatus={() => console.log("status")}
+              onStatus={() => console.log("mudar o status")}
               data={item}
             />
           )}
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,9 +5,9 @@ type ButtonProps = TouchableOpacityProps & {
   title: string;
 };
 
-export default function Button({ title }: ButtonProps) {
+export default function Button({ title, ...rest }: ButtonProps) {
   return (
-    <TouchableOpacity activeOpacity={0.8} style={styles.container}>
+    <TouchableOpacity activeOpacity={0.8} style={styles.container} {...rest}>
       <Text style={styles.title}>{title}</Text>
     </TouchableOpacity>
   );
